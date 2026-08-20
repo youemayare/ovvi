2678851b6ca42ea010da6aeb22e75e6c8726b6aa
youemayare <youemayare@gmail.com>
2026-08-20 12:29:04 +0400

    fix: explicitly route /__clerk in proxy matcher for Vercel auto-proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -68,4 +68,4 @@ export default clerkMiddleware(async (auth, req: NextRequest) => {
   return NextResponse.next();
 });
 
-export const config = { matcher: [ '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)' ] };
+export const config = { matcher: [ '/__clerk(.*)', '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)' ] };
